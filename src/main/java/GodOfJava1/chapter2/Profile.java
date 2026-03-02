5ec49c00c492cf7fe3994e5456f166d228840056
luvc++ <ccccssung@gmail.com>
2026-03-02 18:37:02 +0900

    GodofJava-Chapter2

---
+++ src/main/java/GodOfJava1/chapter2/Profile.java
@@ -0,0 +1,9 @@
+package GodOfJava1.chapter2;
+
+public class Profile {
+    static void main() {
+        System.out.println("My name is " + "Jude");
+        System.out.println("My age is " + 32);
+
+    }
+}
